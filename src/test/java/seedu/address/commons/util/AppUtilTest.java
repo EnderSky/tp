5e28de2b19eaa72a0b9937324b1061aa1c98934a
Jotham Lim <EnderSky@users.noreply.github.com>
2026-03-25 23:44:27 +0800

    Add load image tests

--- src/test/java/seedu/address/commons/util/AppUtilTest.java
+++ src/test/java/seedu/address/commons/util/AppUtilTest.java
@@ -8,13 +8,33 @@ import org.junit.jupiter.api.Test;
 public class AppUtilTest {
 
     @Test
-    public void getImage_exitingImage() {
+    public void getImage_existingImage() {
         assertNotNull(AppUtil.getImage("/images/address_book_32.png"));
     }
 
     @Test
     public void getImage_nullGiven_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> AppUtil.getImage(null));
+    }
+
+    @Test
+    public void loadImage_existingImage() {
+        assertNotNull(AppUtil.loadImage("/images/address_book_32.png"));
+    }
+
+    @Test
+    public void loadImage_nullGiven_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> AppUtil.loadImage(null));
+    }
+
+    @Test
+    public void loadImage_nonExistingImage_loadsFallbackImage() {
+        assertNotNull(AppUtil.loadImage("/images/non_existing_image.png"));
+    }
+
+    @Test
+    public void loadImage_validClasspathImage_success() {
+        assertNotNull(AppUtil.loadImage("/images/placeholder-pet-logo.png"));
     }
 
     @Test
